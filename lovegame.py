d70e4d2ab3db53d9f760d9f76d529079b76db947
Billal Nawabi <BillalNawabi@mbp-van-billal.wifi.hu.nl>
2018-09-28 13:21:25 +0200

    exercise lovegame

---
+++ lovegame.py
@@ -0,0 +1,9 @@
+own_name = input("What is your name? ")
+lovers_name = input("What is your lovers name? ")
+
+own_name_lower = own_name.lower().strip()
+lovers_name_lower = lovers_name.lower().strip()
+
+own_name_lower
+lovers_name_lower
+print(own_name_lower + lovers_name_lower)
